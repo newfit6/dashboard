1167685b73a6a224365d9c2c3e94605b3d31f8ae
newfit6 <newfit6@gmail.com>
2022-02-10 22:18:44 +0900

    add: notification, fix: Nav-bar margin

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import btn_dashboard from "./dashboard.svg";
+import btn_archive from "./archive.svg";
+import btn_journal from "./journal.svg";
+import btn_calendar from "./calendar.svg";
+import btn_network from "./network.svg";
+import btn_search from "./search.svg";
+import btn_report from "./report.svg";
+import btn_settings from "./settings.svg";
 
 function App() {
+  let title = "Dashboard";
+  let noti =
+    "📢  It's 3 days before the registration for the 1st semester of 2022.";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="Title-bar">
+        <div> {title} </div>
+        <div className="Noti-bar">
+          <div> {noti} </div>
+        </div>
+      </div>
+
+      <div className="Nav-bar">
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-dashboard">
+            <img src={btn_dashboard}></img>
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-archive">
+            <img src={btn_archive}></img>
+          </button>
+        </p>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-journal">
+            <img src={btn_journal}></img>
+          </button>
+        </p>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-calendar">
+            <img src={btn_calendar}></img>
+          </button>
+        </p>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-network">
+            <img src={btn_network}></img>
+          </button>
+        </p>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-search">
+            <img src={btn_search}></img>
+          </button>
+        </p>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-report">
+            <img src={btn_report}></img>
+          </button>
+        </p>
+        <p style={{ margin: 0 }}>
+          <button type="button" className="btn-settings">
+            <img src={btn_settings}></img>
+          </button>
+        </p>
+      </div>
+
+      <header className="App-header"></header>
     </div>
   );
 }
