8e4fff4528976bea285a144f030c17c7bb9e601e
newfit6 <newfit6@gmail.com>
2022-02-15 21:52:30 +0900

    add: containter-main__item-dashboard-journal__list--like-box

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 
 import btn_dashboard from "./dashboard.svg";
@@ -8,16 +9,37 @@ import btn_network from "./network.svg";
 import btn_search from "./search.svg";
 import btn_report from "./report.svg";
 import btn_settings from "./settings.svg";
+import btn_like from "./like.svg";
 
 /* Nav-bar icon import from ./ */
 
 function App() {
   let title = "Dashboard";
-  let noti =
-    "📢  It's 3 days before the registration for the 1st semester of 2022.";
-  let Journal = "SampleTag";
+
+  let [noti, noti_change] = useState([
+    "📢  It's 3 days before the registration for the 1st semester of 2022.",
+    "Sample Second Notification",
+  ]);
+
+  let Journal_Archive = "📃 Journal Archive";
 
   /*let from server*/
+
+  let [Journal_Tag, Journal_Tag_Change] = useState([
+    "First Tag",
+    "Second Tag",
+    "Third Tag",
+  ]);
+
+  let [Journal_Title, Journal_Title_Change] = useState([
+    "First useState Data",
+    "Second useState Data",
+    "Third useState Data",
+  ]);
+
+  function function_like() {
+    console.log(1);
+  }
 
   return (
     <div className="container-main">
@@ -25,47 +47,47 @@ function App() {
 
       <div className="container-main__item-header">
         <div className="container-main__item-header--title"> {title} </div>
-        <div className="container-main__item-header--noti"> {noti} </div>
+        <div className="container-main__item-header--noti"> {noti[0]} </div>
       </div>
 
       {/* Nav-bar */}
 
       <div className="container-main__item-nav__top">
-        <p style={{ margin: 0 }}>
+        <div style={{ margin: 0 }}>
           <button type="button" className="container-main__item-nav__top--menu">
             <img src={btn_dashboard} alt="profile"></img>
           </button>
-        </p>
+        </div>
 
-        <p style={{ margin: 0 }}>
+        <div style={{ margin: 0 }}>
           <button type="button" className="container-main__item-nav__top--menu">
             <img src={btn_archive} alt="profile"></img>
           </button>
-        </p>
+        </div>
 
-        <p style={{ margin: 0 }}>
+        <div style={{ margin: 0 }}>
           <button type="button" className="container-main__item-nav__top--menu">
             <img src={btn_journal} alt="profile"></img>
           </button>
-        </p>
+        </div>
 
-        <p style={{ margin: 0 }}>
+        <div style={{ margin: 0 }}>
           <button type="button" className="container-main__item-nav__top--menu">
             <img src={btn_calendar} alt="profile"></img>
           </button>
-        </p>
+        </div>
 
-        <p style={{ margin: 0 }}>
+        <div style={{ margin: 0 }}>
           <button type="button" className="container-main__item-nav__top--menu">
             <img src={btn_network} alt="profile"></img>
           </button>
-        </p>
+        </div>
 
-        <p style={{ margin: 0 }}>
+        <div style={{ margin: 0 }}>
           <button type="button" className="container-main__item-nav__top--menu">
             <img src={btn_search} alt="profile"></img>
           </button>
-        </p>
+        </div>
       </div>
 
       <div className="container-main__item-nav__bottom">
@@ -88,23 +110,61 @@ function App() {
       </div>
 
       <div className="container-main__item-dashboard-journal">
+        <div className="container-main__item-dashboard-journal__title">
+          {Journal_Archive}
+        </div>
+
+        {/*journal list 0 start*/}
         <div className="container-main__item-dashboard-journal__list">
           <div className="container-main__item-dashboard-journal__list--tag">
-            {Journal}
+            {Journal_Tag[0]}
           </div>
           <div className="container-main__item-dashboard-journal__list--title">
-            S1-W1 : Journal List Sample Title
+            {Journal_Title[0]}
+          </div>
+
+          <div
+            className="container-main__item-dashboard-journal__list--like-box"
+            onClick={function_like}
+          >
+            <img
+              className="container-main__item-dashboard-journal__list--like-box--like"
+              src={btn_like}
+              alt="profile"
+            ></img>
+          </div>
+          {/*like event listner*/}
+        </div>
+        {/*journal list 0 end*/}
+
+        {/*journal list 1 start*/}
+        <div className="container-main__item-dashboard-journal__list">
+          <div className="container-main__item-dashboard-journal__list--tag">
+            {Journal_Tag[1]}
+          </div>
+          <div className="container-main__item-dashboard-journal__list--title">
+            {Journal_Title[1]}
           </div>
         </div>
+        {/*journal list 1 end*/}
+
+        {/*journal list 2 start*/}
+        <div className="container-main__item-dashboard-journal__list">
+          <div className="container-main__item-dashboard-journal__list--tag">
+            {Journal_Tag[2]}
+          </div>
+          <div className="container-main__item-dashboard-journal__list--title">
+            {Journal_Title[2]}
+          </div>
+        </div>
+        {/*journal list 2 end*/}
       </div>
 
       {/* Journal-list */}
 
       {/* App-header*/}
 
-      <header className="App-header">
-        <style> @import url('http://fonts.cdnfonts.com/css/gotham'); </style>
-      </header>
+      <style> @import url('http://fonts.cdnfonts.com/css/gotham'); </style>
     </div>
   );
 }
